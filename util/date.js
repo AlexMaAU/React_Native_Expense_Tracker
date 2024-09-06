dcda59509cbcc8ee938418948b0899f763a0d2f4
AlexmaAU <alexma8809@gmail.com>
2024-09-06 23:36:39 +1000

    Add updates

--- util/date.js
+++ util/date.js
@@ -2,7 +2,7 @@ export function getFormattedDate(date) {
   return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
 
-// count days
+// calculate days, within 7 days
 export function getDateMinusDays(date, days) {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
 }
